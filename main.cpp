9ccc64bcd7a725c143a12c42fd416f574a885035
juyonglee <juyonglee0208@gmail.com>
2023-06-13 02:19:49 +0900

    pcap_init 구현

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <pcap/pcap.h>
+#define PCAP_ERR_BUF_SIZE 1024 * 1024
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char errbuf[PCAP_ERR_BUF_SIZE];
+    if (pcap_init(PCAP_CHAR_ENC_UTF_8, errbuf)) {
+        std::cout << "PCAP Init Fail!" << std::endl;
+        std::cout << errbuf << std::endl;
+    }
     return 0;
 }
