90864b83e60a45da27e5152620517cee54588dc0
juyonglee <juyonglee0208@gmail.com>
2023-06-13 02:37:23 +0900

    pcap_findalldevs 구현

--- main.cpp
+++ main.cpp
@@ -2,11 +2,21 @@
 #include <pcap/pcap.h>
 #define PCAP_ERR_BUF_SIZE 1024 * 1024
 
+using namespace std;
+
 int main() {
-    char errbuf[PCAP_ERR_BUF_SIZE];
-    if (pcap_init(PCAP_CHAR_ENC_UTF_8, errbuf)) {
-        std::cout << "PCAP Init Fail!" << std::endl;
-        std::cout << errbuf << std::endl;
+    char errorBuffer[PCAP_ERR_BUF_SIZE];
+
+    if (pcap_init(PCAP_CHAR_ENC_UTF_8, errorBuffer)) {
+        cout << "PCAP Init Fail!" << endl;
+        cout << "Error Message: "<< errorBuffer << endl;
+    }
+
+    pcap_if_t* alldevsp;
+
+    if (pcap_findalldevs(&alldevsp, errorBuffer)) {
+        cout << "Find All Device Fail!" << endl;
+        cout << errorBuffer << endl;
     }
     return 0;
 }
